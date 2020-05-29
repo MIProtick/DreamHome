1b14bfafa269b2b1b55be98a728ed657e9139127
Mezbaul Islam Protick <mezbaulislamprotick@outlook.com>
2020-05-29 23:25:16 +0600

    Initial DB Migrations

--- dreamhome/settings.py
+++ dreamhome/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'dreamhome.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dreamhomedb',
+        'USER': 'postgres',
+        'PASSWORD': 'protick',
+        'HOST': 'localhost'
     }
 }
 
