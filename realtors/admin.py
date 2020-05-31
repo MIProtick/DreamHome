b151b122feee71fa3c27b5c014755a36b8e9d216
Mezbaul Islam Protick <mezbaulislamprotick@outlook.com>
2020-06-01 00:45:13 +0600

    Almost everything except reg and log

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Realtor
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'is_mvp','hire_date')
+    list_display_links = ('id', 'name')
+    list_editable = ('is_mvp',)
+    search_fields = ('name',)
+    list_per_page = 20
+
+admin.site.register(Realtor, RealtorAdmin)
